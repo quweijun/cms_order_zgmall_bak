f7b462968cc3091059302a6a9adbc30876a6d611
quweijun <29048829@qq.com>
2018-12-10 13:37:18 +0800

    Default

---
+++ src/main/java/com/zgmall/config/shiro/UserType.java
@@ -0,0 +1,25 @@
+package com.zgmall.config.shiro;
+
+public class UserType {
+//    /** 经销商平台 */
+//    public static final String AGENCY = "agency";
+//
+//    /** 厂商平台 */
+//    public static final String FACTORY = "factory";
+//
+//    /** 系统平台 */
+//    public static final String SYSTEM = "system";
+//
+//    /** 消费者平台 */
+//    public static final String PERSON = "person";
+//
+//    /** 游客 */
+//    public static final String GUEST = "guest";
+
+    /**商城ERP平台用户**/
+    public static final String ERPUSER="erpuser";
+
+    /**会员**/
+    public static final String CUSTOMER="customer";
+
+}
